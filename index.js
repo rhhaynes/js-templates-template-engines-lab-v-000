67244049d81ccf1f0b130092622119f2dc28bad2
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-31 16:06:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ function createPost(){
   const postTitle    = document.getElementById("postTitle").value;
   const postAuthor   = document.getElementById("postAuthor").value;
   const postText     = document.getElementById("postText").value;
+  
   const postTemplate = document.getElementById("page-template").innerHTML;
 
   const templateFn   = _.template(postTemplate);
-  const templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+  const templateHTML = templateFn({ 'postTitle': postTitle, 'postAuthor': postAuthor, 'postText': postText});
 
   let postDiv = document.getElementById("post");
   postDiv.innerHTML += templateHTML;
