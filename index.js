51dabe7416b3ab5247861ad230ea1d9d0b88500f
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-31 15:49:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function createPost(){
-  bla
+  
+  commenter = document.getElementById("commenterName").value;
+  comment = document.getElementById("commentText").value;
+  commentTemplate = document.getElementById("comment-template").innerHTML;
+ 
+  //create template function
+  var templateFn = _.template(commentTemplate);
+ 
+  var commentsDiv = document.getElementById("comments");
+ 
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+ 
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
 }
 
 function postComment(){}
